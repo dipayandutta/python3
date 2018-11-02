bba7af3874f9a003387f96954e49d95ce54e4fdc
Dipayan Dutta <inbox.dipayan@gmail.com>
2018-11-02 22:15:31 +0530

    Brute Force Craking using python

---
+++ crack.py
@@ -0,0 +1,14 @@
+import time
+import itertools
+password = "P@ssw0rd#"
+print ("Cracking...")
+start = time.time()
+for test in itertools.product('abcdefghijklmnopqrstuvwxyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%^&*1234567890', repeat = 7):
+    print("".join(test))
+    if "".join(test) == password:
+        stop = time.time()
+        print ("".join(test), "match found! Process took {0} seconds".format(stop - start))
+        
+        break
+    else:
+        continue
