377deccb186b6c1e90375460395819a21f259aa4
Dipayan Dutta <inbox.dipayan@gmail.com>
2018-11-04 22:57:54 +0530

    Another Good Example

--- GUI/TkinterSimpleGrid02.py
+++ GUI/TkinterSimpleGrid02.py
@@ -7,6 +7,8 @@ class Example(Frame):
         super().__init__()
         
         self.initUI()
+        self.centreWindow()
+        self.username_password()
 
 
     def initUI(self):
@@ -14,14 +16,36 @@ class Example(Frame):
         self.master.title("Application")
         self.pack()
         
-        self.username_label = Label(self,text="username",width=20)
-        self.username_label.grid(row=0,column=0,sticky=W,padx=10,pady=10)
+        
+    def centreWindow(self):
+        pass
 
+    def username_password(self):
+
+        #---------------------------USERNAME LABEL------------------#
+        self.username_label = Label(self,text="username".upper(),width=20)
+        self.username_label.grid(row=0,column=0,sticky=W,padx=10,pady=10)
+        #----------------------------USERNAME ENTRY------------------#
         self.username_entry = Entry(self)
         self.username_entry.grid(row=0,column=1,padx=10,pady=10)
+        #---------------------------PASSWORD LABEL--------------------#
+        self.password_label = Label(self,text="password".upper(),width=20)
+        self.password_label.grid(row=1,column=0,sticky=W,padx=10,pady=10)
+        #---------------------------PASSWORD ENTRY---------------------#
+        self.password_entry = Entry(self)
+        self.password_entry.grid(row=1,column=1,padx=10,pady=10)
+        #--------------------------LOGIN BUTTON--------------------------#
+        loginButton = Button(self,text="login".upper(),command=self.click)
+        loginButton.grid(row=2,column=1,padx=10,pady=10)
 
+    def click(self):
+        #---------------------------GETTING USERNAME AND PASSWORD VALUE-----------------------#
+        username = self.username_entry.get()
+        password = self.password_entry.get()
 
-
+        print(username)
+        print(password)
+        
 def main():
     root= Tk()
     root.geometry("300x300+200+200")
