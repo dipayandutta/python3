0a4b533d7210dd5506aadbb6be77d89bd5d4cf01
Dipayan Dutta <inbox.dipayan@gmail.com>
2019-02-02 22:08:46 +0530

    django portfolio example code

--- django_protfolio/src/portfolio/jobs/views.py
+++ django_protfolio/src/portfolio/jobs/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 def home(request,*args,**kwargs):
-
+	savedJobs = Job.objects 
 	displayString = {
 
 					"title":"index",
 					"sampleText" : "Hello World!",
+					"jobs" : savedJobs,
 	}
 	return render(request,'jobs/home.html',displayString)
