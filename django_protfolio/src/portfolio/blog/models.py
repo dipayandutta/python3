5e6216ea8159b3403f96fe529e9bce74395cb7a1
Dipayan Dutta <inbox.dipayan@gmail.com>
2019-02-02 23:36:48 +0530

    Portfolio Application Logical Functionality Completed!

--- django_protfolio/src/portfolio/blog/models.py
+++ django_protfolio/src/portfolio/blog/models.py
@@ -6,3 +6,10 @@ class Blog(models.Model):
 	pub_date	= models.DateTimeField()
 	body		= models.TextField()
 	image		= models.ImageField(upload_to='images/')
+	at_a_glance = models.CharField(max_length=100,default='blog summary')
+
+	def summary(self):
+		return self.body[:100]
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
--- django_protfolio/src/portfolio/blog/views.py
+++ django_protfolio/src/portfolio/blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from .models import Blog
+from django.shortcuts import get_object_or_404
 # Create your views here.
+def allblogs(request,*args,**kwargs):
+	blogDB = Blog.objects
+	displayString = {
+						"title":"Blog Application",
+						"blogField":blogDB,
+
+	}
+	return render(request,'blog/allblogs.html',displayString)
+
+
+
+def detial(request,blog_id):
+	blogDetails = get_object_or_404(Blog,pk=blog_id)
+	displayFullBlog = {
+
+						"title": "Full Page",
+						"detail": blogDetails,
+	}
+	return render(request,'blog/detail.html',displayFullBlog)
+
+	
