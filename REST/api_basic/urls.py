6d7cccb15f407cb43bff36c378a9aedaab7f6437
Dipayan Dutta <inbox.dipayan@gmail.com>
2021-06-14 00:12:17 +0530

    Generic View added

--- REST/api_basic/urls.py
+++ REST/api_basic/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path 
-from .views import article_list,article_detail,ArticleAPIView,ArticleDetails
+from .views import (article_list,article_detail,ArticleAPIView,ArticleDetails,GenericAPIView,
+		GenericAPIListall)
+
 
 urlpatterns = [
 	path('article/',article_list),
 	path('articleclass/',ArticleAPIView.as_view()),
 	path('articledetails/<int:id>/',ArticleDetails.as_view()),
-	path('detail/<int:pk>/',article_detail)
+	path('detail/<int:pk>/',article_detail),
+	path('genericlist/<int:id>/',GenericAPIView.as_view()),
+	path('genericlistall/',GenericAPIListall.as_view())
 		
 ]
 
--- REST/api_basic/views.py
+++ REST/api_basic/views.py
@@ -15,9 +15,59 @@ from rest_framework import status
 for class based view
 '''
 from rest_framework.views import APIView
+
+'''
+for generic based view
+'''
+from rest_framework import generics
+from rest_framework import mixins
 # Create your views here.
+'''
+	Example of Class Based View
+'''
+
+class GenericAPIListall(generics.GenericAPIView,mixins.ListModelMixin):
+
+	serializer_class = ArticleSerializer
+	queryset = Article.objects.all()
+
+	def get(self,request):
+		return self.list(request)
 
 
+class GenericAPIView(generics.GenericAPIView,mixins.ListModelMixin,mixins.CreateModelMixin,
+	mixins.UpdateModelMixin,mixins.RetrieveModelMixin,mixins.DestroyModelMixin):
+
+	serializer_class = ArticleSerializer
+	queryset = Article.objects.all()
+
+	lookup_field = 'id' # required for update the data put method
+
+	def get(self,request,id):
+		if id:
+			return self.retrieve(request)
+		else:
+			return self.list(request)
+
+	# For Posting Data
+
+	def post(self,request):
+		return self.create(request)
+
+	# For updating data
+	def put(self,request,id=None):
+		return self.update(request,id)
+
+
+	# For Delete Method
+	def delete(self,request,id):
+		return self.destroy(request,id)
+
+
+
+'''
+	Example of Class Based View
+'''
 class ArticleAPIView(APIView):
 	def get(self,request):
 		articles 	= Article.objects.all()# get all the data from the database
@@ -60,6 +110,9 @@ class ArticleDetails(APIView):
 		return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+'''
+	Example of function based views
+'''
 #@csrf_exempt
 @api_view(['GET','POST']) #for api_view
 def article_list(request):
