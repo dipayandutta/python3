46aa633c116d44d51a8af0dd5756bf934ec0ea34
Dipayan Dutta <inbox.dipayan@gmail.com>
2021-06-15 00:26:19 +0530

    added some more admin customization

--- adminCustomization/blog/admin.py
+++ adminCustomization/blog/admin.py
@@ -1,10 +1,36 @@
 from django.contrib import admin
-from . import models
+from .models import post,Category
+
 # Register your models here.
 class BlogAdminArea(admin.AdminSite):
 	site_header = 'Blog Admin Area'
 
-blog_site = BlogAdminArea(name='Blog Admin')
+#blog_site = BlogAdminArea(name='Blog Admin')
 
-blog_site.register(models.post)
-admin.site.register(models.post)
+#blog_site.register(post)
+
+TEXT = 'Some Text I Write'
+
+class PostAdmin(admin.ModelAdmin):
+	fieldsets = (
+
+		('Section1',{
+
+			'fields':('title','author'),
+			'description': '%s' % TEXT,
+			}),
+		('Section2',{
+			'fields':('slug',),
+			'classes':('collapse',),
+			})
+		)
+
+admin.site.register(post,PostAdmin)
+admin.site.register(Category)
+
+'''
+class PostAdmin(admin.ModelAdmin):
+	fields = ['title','author']
+
+admin.site.register(post, PostAdmin)
+'''
--- adminCustomization/blog/models.py
+++ adminCustomization/blog/models.py
@@ -1,9 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name 
+
+
 class post(models.Model):
 
-	title = models.CharField(max_length=100)
+	options = (
+		('draft','Draft'),
+		('published','Published'),
+		)
 
+	category = models.ForeignKey(Category,on_delete=models.PROTECT,default=1)
+	title = models.CharField(max_length=200,null=True)
+	excerpt = models.TextField(null=True)
+	slug = models.SlugField(max_length=250,unique_for_date='publish',null=True)
+	author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts',default=1)
+	publish = models.DateTimeField(default=timezone.now)
+	content = models.TextField(null=True)
+	status = models.CharField(max_length=10,choices=options,default='draft')
+
+	
 	def __str__(self):
 		return self.title
