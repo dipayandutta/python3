9e66b8b3facf56e32573f1187c5fbb57e9689988
Dipayan Dutta <inbox.dipayan@gmail.com>
2021-06-14 22:29:39 +0530

    Adding multiple Admin in the project

--- adminCustomization/bookstore/admin.py
+++ adminCustomization/bookstore/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+class BookStoreAdminArea(admin.AdminSite):
+	site_header = 'Bookstore Admin Area'
+
+
+bookstore_site = BookStoreAdminArea(name='Bookstore Admin')
