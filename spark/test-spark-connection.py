4538cab9af89a1687ee4107102cbdeb93970ba1b
Dipayan Dutta <inbox.dipayan@gmail.com>
2021-07-18 22:03:28 +0530

    Spark Basics

--- spark/test-spark-connection.py
+++ spark/test-spark-connection.py
@@ -1,5 +1,8 @@
+import findspark
 from pyspark.sql import SparkSession
-
+from pyspark.conf import SparkConf
+findspark.init()
+SparkSession.builder.config(conf=SparkConf())
 sc = SparkSession.builder.appName('helloworld').getOrCreate()
 
 print(type(sc),"\n")
